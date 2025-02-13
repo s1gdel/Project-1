f0034bb745f71e818ee8460e922843999b057bb2
s1gdel <subhamsigdel@gmail.com>
2025-02-13 17:06:59 -0600

    Update SinglyLL.java

--- SinglyLL.java
+++ SinglyLL.java
@@ -1,37 +1,52 @@
 public class SinglyLL<AnyType> implements IDedObject {
     private Node<AnyType> head;
-    private int size;
     private Product product;
 
     public SinglyLL() {
         this.head = null;
-        this.size = 0;
         this.product = new Product();
     }
 
 
-    void makeEmpty() {
+    public void makeEmpty() {
+    
+    }
+
+    public AnyType findID(int ID) {
         
     }
 
-    AnyType findID(int ID) {
+    public boolean insertAtFront(AnyType x) {
+
+        if(head == null){
+            //this is the first node
+        }
+        else{
+            //this is not the first node
+        }
         
     }
 
-    boolean insertAtFront(AnyType x) {
+    public AnyType deleteFromFront() {
         
     }
 
-    AnyType deleteFromFront() {
+    public AnyType delete(int ID) {
         
     }
 
-    AnyType delete(int ID) {
-        
-    }
-
-    void printAllRecords() {
-        
+    public void printAllRecords() {
+        if(head == null){
+            System.out.println("Linked list is empty");
+        }
+        else{
+            Node<AnyType> current = head;
+            while(current != null){
+                System.out.print(current.getData() + "->");
+                current = current.getNextNode();
+            }
+            System.out.print("NULL");
+        }
     }
 
 
