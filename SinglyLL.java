f134166c7625887dfc2ca01117854ddc4dba903d
s1gdel <subhamsigdel@gmail.com>
2025-02-18 14:30:39 -0600

    Update SinglyLL.java

--- SinglyLL.java
+++ SinglyLL.java
@@ -7,30 +7,72 @@ public class SinglyLL<AnyType extends IDedObject>  {
 
 
     public void makeEmpty() {
-    
+        if(head !=null){
+            head = null;
+        }else{
+            System.out.println("Linked list is empty");
+        }
     }
 
     public AnyType findID(int ID) {
-        
+       Node<AnyType> current = head;
+       while(current != null){
+        if(current.getData() != null && current.getData().getID() == ID){
+
+            current.getData().printID();
+            return current.getData();
+        }
+        current = current.getNextNode();
+       }
+       return null;
     }
 
-    public boolean insertAtFront(AnyType x) {
+    /*public boolean insertAtFront(AnyType x) {
 
-        if(head == null){
-            //this is the first node
+        if(){
+            //call id finds id return false w/msg id has already been found
         }
         else{
-            //this is not the first node
+            Node <AnyType> newNode = new Node <AnyType>(x);
+            newNode.setNextNode(head);
+            head=newNode;
+            return true;
         }
-        
-    }
+    }*/
 
     public AnyType deleteFromFront() {
-        
+        if(head==null){
+            return null;
+        }else{
+            Node <AnyType> headDisplay = head;
+            AnyType nodeData = head.getData();
+            headDisplay.getData().printID();;
+            head=head.getNextNode();
+            return nodeData;
+        }
     }
 
     public AnyType delete(int ID) {
-        
+        Node<AnyType> prev=null;
+        Node <AnyType> current = head;
+        while(current !=null){
+            if(current.getData() != null && current.getData().getID() == ID){
+                AnyType nodeData = current.getData();
+                current.getData().printID();
+                if(prev == null){
+                    head=current.getNextNode();
+                }
+                else{
+                    prev.setNextNode(current.getNextNode());
+                }
+                return nodeData;
+            }
+            else{
+              prev = current;
+              current = current.getNextNode();
+            }
+        }
+        return null;
     }
 
     public void printAllRecords() {
